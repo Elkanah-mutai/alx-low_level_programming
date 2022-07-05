9fd2b76b207f1b660d57e8354c04036f87e1a770
Elkanah-mutai <elkmutai@gmail.com>
2022-07-05 13:47:02 -0700

    function that prints every minute of the day of Jack Baue

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,15 @@
+#include <limits.h>
 #include"main.h"
 /**
  * print_last_digit - prints the last digit of a numer.
  *@n: digit to find the last place of.
  *Return: The last digit.
  */
-int print_last_digit(int n)
+int main(void)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int r;
+	r = print_last_digit(INT_MIN);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
